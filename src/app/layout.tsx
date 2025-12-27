3be56d921c44226c19e226731dce1045990f0e0f
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-12-27 21:10:09 +0100

    wip typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const nunitoSans = Nunito_Sans({variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const nunitoSans = Nunito_Sans({ variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,12 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={nunitoSans.variable}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={cn(nunitoSans.variable, "antialiased")}>
+      <body>{children}</body>
     </html>
   );
 }
---
+++ src/components/typography.tsx
@@ -0,0 +1,5 @@
+export const Typography = () => {
+  return (
+    <div>hello world</div>
+  )
+}
