9ae720d7d37c18a925ad987ef8cda9bcb4e47fed
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-12-27 22:20:07 +0100

    set container max width

--- src/components/theme/container.tsx
+++ src/components/theme/container.tsx
@@ -6,11 +6,15 @@ interface ContainerProps {
 }
 
 export const Narrow = ({ children, className }: ContainerProps) => {
-  return <div className={cn("px-8 sm:px-16", className)}>{children}</div>;
+  return (
+    <div className={cn("px-8 max-w-2xl mx-auto", className)}>{children}</div>
+  );
 };
 
 export const Wide = ({ children, className }: ContainerProps) => {
-  return <div className={cn("px-8", className)}>{children}</div>;
+  return (
+    <div className={cn("px-8 max-w-4xl mx-auto", className)}>{children}</div>
+  );
 };
 
 export const Full = ({ children, className }: ContainerProps) => {
