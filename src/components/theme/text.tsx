0b18ac1536c8263a665f3f2c1985c2c6daf6d651
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-12-27 21:53:00 +0100

    allow override

--- src/components/theme/text.tsx
+++ src/components/theme/text.tsx
@@ -1,31 +1,34 @@
+import { cn } from "@/lib/utils";
+
 interface TextProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-export const H1 = ({ children }: TextProps) => {
-  return <h1 className="text-4xl font-bold">{children}</h1>;
+export const H1 = ({ children, className }: TextProps) => {
+  return <h1 className={cn("text-4xl font-bold", className)}>{children}</h1>;
 };
 
-export const H2 = ({ children }: TextProps) => {
-  return <h2 className="text-3xl font-bold">{children}</h2>;
+export const H2 = ({ children, className }: TextProps) => {
+  return <h2 className={cn("text-3xl font-bold", className)}>{children}</h2>;
 };
 
-export const H3 = ({ children }: TextProps) => {
-  return <h3 className="text-2xl font-bold">{children}</h3>;
+export const H3 = ({ children, className }: TextProps) => {
+  return <h3 className={cn("text-2xl font-bold", className)}>{children}</h3>;
 };
 
-export const H4 = ({ children }: TextProps) => {
-  return <h4 className="text-xl font-bold">{children}</h4>;
+export const H4 = ({ children, className }: TextProps) => {
+  return <h4 className={cn("text-xl font-bold", className)}>{children}</h4>;
 };
 
-export const H5 = ({ children }: TextProps) => {
-  return <h5 className="text-lg font-bold">{children}</h5>;
+export const H5 = ({ children, className }: TextProps) => {
+  return <h5 className={cn("text-lg font-bold", className)}>{children}</h5>;
 };
 
-export const H6 = ({ children }: TextProps) => {
-  return <h6 className="text-base font-bold">{children}</h6>;
+export const H6 = ({ children, className }: TextProps) => {
+  return <h6 className={cn("text-base font-bold", className)}>{children}</h6>;
 };
 
-export const P = ({ children }: TextProps) => {
-  return <p className="text-base">{children}</p>;
+export const P = ({ children, className }: TextProps) => {
+  return <p className={cn("text-base", className)}>{children}</p>;
 };
