4ce9a7ff6c18f08c8fde8afd0a79ff279969f348
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-12-30 16:59:24 +0100

    Add perma prop to headers

--- src/components/theme/text.tsx
+++ src/components/theme/text.tsx
@@ -5,37 +5,41 @@ interface TextProps {
   className?: string;
 }
 
-export const H1 = ({ children, className }: TextProps) => {
+interface HeadingProps extends TextProps {
+  perma?: string
+}
+
+export const H1 = ({ children, className }: HeadingProps) => {
   return (
     <h1 className={cn("text-4xl font-bold mb-5", className)}>{children}</h1>
   );
 };
 
-export const H2 = ({ children, className }: TextProps) => {
+export const H2 = ({ children, className }: HeadingProps) => {
   return (
     <h2 className={cn("text-3xl font-bold mb-3", className)}>{children}</h2>
   );
 };
 
-export const H3 = ({ children, className }: TextProps) => {
+export const H3 = ({ children, className }: HeadingProps) => {
   return (
     <h3 className={cn("text-2xl font-bold mb-2", className)}>{children}</h3>
   );
 };
 
-export const H4 = ({ children, className }: TextProps) => {
+export const H4 = ({ children, className }: HeadingProps) => {
   return (
     <h4 className={cn("text-xl font-bold mb-2", className)}>{children}</h4>
   );
 };
 
-export const H5 = ({ children, className }: TextProps) => {
+export const H5 = ({ children, className }: HeadingProps) => {
   return (
     <h5 className={cn("text-lg font-bold mb-2", className)}>{children}</h5>
   );
 };
 
-export const H6 = ({ children, className }: TextProps) => {
+export const H6 = ({ children, className }: HeadingProps) => {
   return (
     <h6 className={cn("text-base font-bold mb-2", className)}>{children}</h6>
   );
