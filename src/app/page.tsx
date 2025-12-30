c5cb604503068e72ff0b4ad2a3e12a721fda176b
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-12-30 15:52:57 +0100

    Add entry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,28 @@ import { H1, H2, Lead, P } from "@/components/theme/text";
 
 export default function Page() {
   return (
-    <div className="py-12">
-      <Narrow className="mb-10">
-        <H1>Parkgarden</H1>
-        <Lead>
-          Let&apos;s raise a community garden on the Parklane site, where
-          neighbors can grow vegetables, herbs, fruits and flowers together.
-        </Lead>
-      </Narrow>
-      <Narrow>
-        <H2>Dec 27, 2025</H2>
-        <P>We&apos;re live!</P>
-      </Narrow>
+    <div className="py-12 space-y-10">
+      <section>
+        <Narrow>
+          <H1>Parkgarden</H1>
+          <Lead>
+            Let&apos;s raise a community garden on the Parklane site, where
+            neighbors can grow vegetables, herbs, fruits and flowers together.
+          </Lead>
+        </Narrow>
+      </section>
+      <section>
+        <Narrow>
+          <H2>Dec 30, 2025</H2>
+          <P>TODO</P>
+        </Narrow>
+      </section>
+      <section>
+        <Narrow>
+          <H2>Dec 27, 2025</H2>
+          <P>We&apos;re live!</P>
+        </Narrow>
+      </section>
     </div>
   );
 }
