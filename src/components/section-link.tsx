3850f537fb891647f596ed270e77be6843fb5b24
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2025-12-30 17:46:43 +0100

    Write section link to clipboard

--- src/components/section-link.tsx
+++ src/components/section-link.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 
 export interface Props {
@@ -11,8 +13,14 @@ export const SectionLink = ({ perma, children, className }: Props) => {
     return children;
   }
 
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (navigator && navigator.clipboard) {
+      navigator.clipboard.writeText(`${e.currentTarget.href}`);
+    }
+  };
+
   return (
-    <a href={`#${perma}`} className="group relative">
+    <a href={`#${perma}`} className="group relative" onClick={handleClick}>
       <span
         className={cn(
           "absolute -translate-x-full pr-1 opacity-0 transition-opacity group-hover:opacity-20",
